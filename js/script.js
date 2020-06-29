1bc8c3912fde9ab4ab984bf25ce840f82ac164e5
Нина Волчек <mysha76@mail.ru>
2020-06-29 15:44:50 +0300

    Реализован спиннер.

--- js/script.js
+++ js/script.js
@@ -6,8 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
     cartBtn = document.getElementById('cart'),
     wishlistBtn = document.getElementById('wishlist'),
     goodsWrapper = document.querySelector('.goods-wrapper'),
-    cart = document.querySelector('.cart');
-  
+    cart = document.querySelector('.cart'),
+    category = document.querySelector('.category');
+
+  // spinner
+  const loading = () => {
+    goodsWrapper.innerHTML = `
+          <div id="spinner"><div class="spinner-loading">
+          <div><div><div></div></div><div><div></div></div>
+          <div><div></div></div><div><div></div></div></div></div></div>
+          `
+  }
+
+  // Формирование карточки товара
+
   const createCardGoods = (id, title, price, img) => {
     const card = document.createElement('div');
     card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
@@ -27,26 +39,58 @@ document.addEventListener('DOMContentLoaded', () => {
       </div>
     `;
     return card;
-  }
-  
-  goodsWrapper.appendChild(createCardGoods(1, 'Дартс', 1000, './img/temp/Archer.jpg'));
-  goodsWrapper.appendChild(createCardGoods(2, 'Фламинго', 2000, './img/temp/Flamingo.jpg'));
-  goodsWrapper.appendChild(createCardGoods(3, 'Носки', 300, './img/temp/Socks.jpg'));
+  };
 
+  // Перебор и создание всех карточек
 
-  const closeCart = (event) => {
+  const renderCard = items => {
+    goodsWrapper.textContent = '';
+    items.forEach(({ id, title, price, imgMin }) => {
+      goodsWrapper.append(createCardGoods(id, title, price, imgMin));
+    });
+  };
+
+  // Открытие и закрытие модального окна корзины покупок
+  const openCart = event => {
+    event.preventDefault();
+    cart.style.display = 'flex';
+    document.addEventListener('keyup', closeCart);
+  };
+
+  const closeCart = event => {
     const target = event.target;
-    if (target === cart || target.classList.contains('cart-close')) {
+    if (target === cart || target.classList.contains('cart-close') || event.keyCode === 27) {
       cart.style.display = '';
+      document.removeEventListener('keyup', closeCart);
     }
   };
 
-  const openCart = () => {
-    cart.style.display = 'flex';
+  // Получение информации о товарах из базы данных
+  const getGoods = (handler, filter) => {
+    loading();
+    fetch('db/db.json')
+      .then(response => response.json())
+      .then(filter)
+      .then(handler);
+  };
+
+  const randomSort = item => item.sort(() => Math.random() - 0.5);
+
+  const choiceCategory = event => {
+    event.preventDefault();
+    const target = event.target;
+
+    if (target.classList.contains('category-item')) {
+      const category = target.dataset.category;
+      getGoods(renderCard, goods => goods.filter(item => item.category.includes(category)));
+    }
   };
 
   cartBtn.addEventListener('click', openCart);
   cart.addEventListener('click', closeCart);
+  category.addEventListener('click', choiceCategory);
+
+  getGoods(renderCard, randomSort);
 
 
 
